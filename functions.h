51fc17b8260bb58089b7b6b90dfd6bd573fabf30
Darren Bowers <darren.bowers@g.austincc.edu>
2021-09-27 18:15:08 -0500

    updated include and using, prototyped test functions

--- functions.h
+++ functions.h
@@ -7,6 +7,17 @@ Purpose: Program to import a file from command line and analyze its contents.
 #ifndef COMMANDLINE_FUNCTIONS_H
 #define COMMANDLINE_FUNCTIONS_H
 
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::fstream;
+using std::string;
+
+int count_characters(string);
+void display_file(fstream&);
 
 #endif //COMMANDLINE_FUNCTIONS_H
 
