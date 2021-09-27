38205456944312ef4597a328308d9917dedfd16f
Darren Bowers <darren.bowers@g.austincc.edu>
2021-09-27 17:10:49 -0500

    included fstream and functions.h in main.h and declared commands, added if else statement to main.cpp and test code

--- main.cpp
+++ main.cpp
@@ -8,8 +8,14 @@ Purpose: Program to import a file from command line and analyze its contents.
 #include "main.h"
 
 int main(int argc, char** argv) {
-
-
+    fstream myfile;
+    if(argc == 2){
+        myfile.open(argv[1]);
+        cout << "it worked" << endl;
+        myfile.close();
+    } else {
+        cout << "it didn't work" << endl;
+    }
 
     return 0;
 }
--- main.h
+++ main.h
@@ -4,12 +4,17 @@ Coding 03
 Purpose: Program to import a file from command line and analyze its contents.
 **********************/
 
-
-
 #ifndef MAIN_H
 #define MAIN_H
 
 #include <iostream>
+#include <fstream>
+#include "functions.h"
+
+using std::cout;
+using std::endl;
+using std::fstream;
+
 
 #endif /* MAIN_H */
 
